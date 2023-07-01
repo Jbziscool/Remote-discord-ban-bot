54df792c688bb5e17b27e90241083178a7721501
Aleksi <108367033+Jbziscool@users.noreply.github.com>
2023-07-01 20:07:27 +0100

    Update robloxchecker.lua

--- robloxchecker.lua
+++ robloxchecker.lua
@@ -1,12 +1,13 @@
 local HttpService = game:GetService("HttpService")
-local renderurl = ''
+local apiurl = 'http://127.0.0.1:3000'
+local apikey = 'hotdog123'
+
 
 
 game.Players.PlayerAdded:Connect(function(plr)
-	local url = renderurl.."/checkUser?userid="..plr.UserId
+	local url = apiurl.."/checkuser?userid="..plr.UserId.."&api_key="..apikey
 	local response = HttpService:JSONDecode(HttpService:GetAsync(url))
-	if response["banned"] == true then
-		plr:Kick('Permanently banned | Reason: '..response['reason'])
-		plr:Destroy()
+	if response["Banned"] == true then
+		plr:Kick('You are banned | Reason: '..response['reason'])
 	end
-end)
+end)	
