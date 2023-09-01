bf908e73e797f67a44a87ee82abe261b591b6c19
Aleksi <108367033+Jbziscool@users.noreply.github.com>
2023-09-02 01:53:52 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,14 +8,14 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-apiurl = os.getenv('apiurl')
+apiurl = os.getenv('apiurl') #gets things from env file
 bottoken = os.getenv('bottoken')
 
 
 
 API_ENDPOINT = "https://users.roblox.com/v1/usernames/users"
 
-def getUserId(username):
+def getUserId(username): #this is a function to send a request to a api endpoint which gets info
 
     requestPayload = {
         "usernames": [
@@ -37,23 +37,23 @@ def getUserId(username):
 
 
 
-bot = commands.Bot(command_prefix=">", intents=discord.Intents.all())
+bot = commands.Bot(command_prefix=">", intents=discord.Intents.all()) #declare the bot variable for the discord.py
 
 
 @bot.command()
-async def gameban(ctx, name,*, reason):
+async def gameban(ctx, name,*, reason): # name,*, reason, the ,*, meaning that the next argument will repeat no matter how many args
     uri = apiurl
-    client = MongoClient(uri)
+    client = MongoClient(uri) #open a mongodb client instance
 
-    userid = getUserId(name)
+    userid = getUserId(name) #use the getuserid function which will return the user di
 
     try:
         client.server_info()
-        database = client['bandb']
+        database = client['bandb'] #get the client database name thing i am so tired rn god
         collection = database['users']
-        result = collection.insert_one({'userid': f'{userid}', 'username': name, 'reason': reason})
-    finally:
-        client.close()
+        result = collection.insert_one({'userid': f'{userid}', 'username': name, 'reason': reason}) #inserts data into the db
+    finally: #does this last
+        client.close() #close the mongodb client
     await ctx.send(f'Added `{name}` - `{userid}` to the database with reason: `{reason}`')
 
 
@@ -62,18 +62,18 @@ async def gameban(ctx, name,*, reason):
 @bot.command()
 async def gameunban(ctx, user):
     uri = apiurl
-    client = MongoClient(uri)
+    client = MongoClient(uri) #declares mongodb client instance
 
 
     try:
         client.server_info()
-        database = client['bandb']
-        collection = database['users']
-        myquery = { "username": user}
-        collection.delete_one(myquery)
-    finally:
-        client.close()
+        database = client['bandb'] #declare the mongodb database name
+        collection = database['users'] #delcare the collection name
+        myquery = { "username": user} # this is what we want to search for
+        collection.delete_one(myquery) # search through the collection and delete this
+    finally: #lastly
+        client.close() #close the mongodb client
     await ctx.send(f'Removed {user} from the ban database')
 
 
-bot.run(bottoken)
+bot.run(bottoken) #start the bot
