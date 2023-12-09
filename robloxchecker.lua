20f0b587e535dab432fcc1ece3ea62d6aab7b0ed
Jbziscool <108367033+Jbziscool@users.noreply.github.com>
2023-12-09 22:15:07 +0200

    Update robloxchecker.lua

--- robloxchecker.lua
+++ robloxchecker.lua
@@ -6,6 +6,8 @@ local apikey = 'hotdog123'
 
 game.Players.PlayerAdded:Connect(function(plr)
 	local url = apiurl.."/checkuser?userid="..plr.UserId.."&api_key="..apikey
+	local time = math.random(2, 6)
+	wait(time)
 	local response = HttpService:JSONDecode(HttpService:GetAsync(url))
 	if response["Banned"] == true then
 		plr:Kick('You are banned | Reason: '..response['reason'])
